c0628ac54f8f6675ac797fee62213c992cc3a229
LemonHX <runzhe2001@hotmail.com>
2020-12-03 15:32:28 +0800

    🍋 Shader编译

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,9 @@
 pub mod window;
+
+#[allow(dead_code)]
+#[inline(always)]
+pub fn cast_slice<T>(data: &[T]) -> &[u8] {
+    use std::{mem::size_of, slice::from_raw_parts};
+
+    unsafe { from_raw_parts(data.as_ptr() as *const u8, data.len() * size_of::<T>()) }
+}
